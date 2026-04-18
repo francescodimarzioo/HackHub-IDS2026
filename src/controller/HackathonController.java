a06e5c4c4089fc2d9740d19cde0fea5fc7ec1a59
Francesco Di Marzio <francesco.dimarzio@studenti.unicam.it>
2026-04-18 12:10:58 +0200

    aggiornato HackathonController iterazione 3

--- src/controller/HackathonController.java
+++ src/controller/HackathonController.java
@@ -45,4 +45,31 @@ public class HackathonController {
         hackathonValidator.validateInputAvviaFase(idHackathon, idOrganizzatore);
         return hackathonService.concludiFaseSvolgimento(idHackathon, idOrganizzatore);
     }
+
+    public Hackathon proclamaVincitore(Long idHackathon, Long idTeamVincitore, Long idOrganizzatore) {
+        hackathonValidator.validateInputProclamaVincitore(idHackathon, idTeamVincitore, idOrganizzatore);
+        return hackathonService.proclamaVincitore(idHackathon, idTeamVincitore, idOrganizzatore);
+    }
+
+    public void erogaPremio(Long idHackathon) {
+        hackathonService.erogaPremio(idHackathon);
+    }
+
+    public Hackathon modificaHackathon(Long idHackathon, String nome, String regolamento,
+                                       String luogo, Double premioInDenaro,
+                                       int dimensioneMaxTeam, Long idOrganizzatore) {
+        hackathonValidator.validateInputModificaHackathon(idHackathon, idOrganizzatore);
+        return hackathonService.modificaHackathon(idHackathon, nome, regolamento,
+                luogo, premioInDenaro,
+                dimensioneMaxTeam, idOrganizzatore);
+    }
+
+    public Hackathon aggiungiMentore(Long idHackathon, Mentore mentore, Long idOrganizzatore) {
+        hackathonValidator.validateInputAggiungiMentore(idHackathon, mentore.getId(), idOrganizzatore);
+        return hackathonService.aggiungiMentore(idHackathon, mentore, idOrganizzatore);
+    }
+
+    public void verificaScadenzaIscrizioni() {
+        hackathonService.verificaScadenzaIscrizioni();
+    }
 }
