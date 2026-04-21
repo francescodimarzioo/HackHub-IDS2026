eeea97cee6dde52283c5854c332616d6611fabc3
Fabiana Felicioni <fabian.felicioni@stidenti.unicam.it>
2026-04-21 18:27:16 +0200

    correzione Segnalazione

--- src/main/java/com/hackhub/model/Segnalazione.java
+++ src/main/java/com/hackhub/model/Segnalazione.java
@@ -25,4 +25,35 @@ public class Segnalazione {
     private MembroTeam segnalante;
 
     @ManyToOne
-    @JoinColumn(name = "memb
+    @JoinColumn(name = "membro_staff_id")
+    private MembroStaff membroStaff;
+
+    @ManyToOne
+    @JoinColumn(name = "hackathon_id")
+    private Hackathon hackathon;
+
+    public Segnalazione() {}
+
+    public Segnalazione(Long id, String descrizione, MembroTeam segnalante, Hackathon hackathon) {
+        this.id = id;
+        this.descrizione = descrizione;
+        this.segnalante = segnalante;
+        this.hackathon = hackathon;
+        this.dataInvio = LocalDateTime.now();
+        this.stato = "IN_ATTESA";
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getDescrizione() { return descrizione; }
+    public void setDescrizione(String descrizione) { this.descrizione = descrizione; }
+    public LocalDateTime getDataInvio() { return dataInvio; }
+    public String getStato() { return stato; }
+    public void setStato(String stato) { this.stato = stato; }
+    public MembroTeam getSegnalante() { return segnalante; }
+    public void setSegnalante(MembroTeam segnalante) { this.segnalante = segnalante; }
+    public MembroStaff getMembroStaff() { return membroStaff; }
+    public void setMembroStaff(MembroStaff membroStaff) { this.membroStaff = membroStaff; }
+    public Hackathon getHackathon() { return hackathon; }
+    public void setHackathon(Hackathon hackathon) { this.hackathon = hackathon; }
+}
