2deea73fa33b7b93dab83a9b57fe71d371934d0b
mandydb04 <mariadeledibiase04@gmail.com>
2026-04-22 15:08:56 +0200

    Iterazione 4: IHackathonRepository e IUtenteRepository Spring Data JPA

--- src/main/java/com/hackhub/repository/IValutazioneRepository.java
+++ src/main/java/com/hackhub/repository/IValutazioneRepository.java
@@ -1,19 +1,16 @@
 package com.hackhub.repository;
 
-import com.hackhub.model.*;
-
-import com.hackhub.model.Hackathon;
-import com.hackhub.model.Sottomissione;
 import com.hackhub.model.Valutazione;
+import com.hackhub.model.Sottomissione;
+import com.hackhub.model.Hackathon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
-public interface IValutazioneRepository {
-
-    Valutazione findById(Long id);
-
-    Valutazione findBySottomissione(Sottomissione sottomissione);
-
+@Repository
+public interface IValutazioneRepository extends JpaRepository<Valutazione, Long> {
+    Optional<Valutazione> findBySottomissione(Sottomissione sottomissione);
     List<Valutazione> findByHackathon(Hackathon hackathon);
-
-    Valutazione save(Valutazione valutazione);
 }
