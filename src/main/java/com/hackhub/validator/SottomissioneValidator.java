2fc9f11a29791912dbab6858eb730307891355ca
Francesco Di Marzio <francesco.dimarzio@studenti.unicam.it>
2026-04-22 16:18:50 +0200

    Iterazione 4: aggiunta Component sprin su SottomissioneValidator HackathonValidator AuthValidator

--- src/main/java/com/hackhub/validator/SottomissioneValidator.java
+++ src/main/java/com/hackhub/validator/SottomissioneValidator.java
@@ -2,7 +2,9 @@ package com.hackhub.validator;
 
 import com.hackhub.model.*;
 import com.hackhub.dto.*;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SottomissioneValidator {
 
     public void validateInputInvia(Long idTeam, Long idHackathon,
