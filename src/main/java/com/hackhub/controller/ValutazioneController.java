c9a53f2557265691d0303d0573c463ef5d4b975c
mandydb04 <mariadeledibiase04@gmail.com>
2026-04-22 15:50:31 +0200

    Iterazione 4: TeamController ValutazioneController con annotazioni Spring

--- src/main/java/com/hackhub/controller/ValutazioneController.java
+++ src/main/java/com/hackhub/controller/ValutazioneController.java
@@ -3,29 +3,29 @@ package com.hackhub.controller;
 import com.hackhub.model.*;
 import com.hackhub.service.*;
 import com.hackhub.validator.*;
-import com.hackhub.dto.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import com.hackhub.model.Giudice;
-import com.hackhub.model.Valutazione;
-import com.hackhub.service.ValutazioneService;
-import com.hackhub.validator.ValutazioneValidator;
-
+@RestController
+@RequestMapping("/valutazione")
 public class ValutazioneController {
 
+    @Autowired
     private ValutazioneService valutazioneService;
+
+    @Autowired
     private ValutazioneValidator valutazioneValidator;
 
-    public ValutazioneController(ValutazioneService valutazioneService,
-                                 ValutazioneValidator valutazioneValidator) {
-        this.valutazioneService = valutazioneService;
-        this.valutazioneValidator = valutazioneValidator;
-    }
-
-    public Valutazione valutaSottomissione(Long idSottomissione, String giudizio,
-                                           int punteggio, Giudice giudice) {
-        valutazioneValidator.validateInputUtente(idSottomissione, giudizio,
-                punteggio, giudice.getId());
-        return valutazioneService.valutaSottomissione(idSottomissione, giudizio,
-                punteggio, giudice);
+    @PostMapping("/valuta")
+    public ResponseEntity<Valutazione> valutaSottomissione(@RequestParam Long idSottomissione,
+                                                           @RequestParam String giudizio,
+                                                           @RequestParam int punteggio,
+                                                           @RequestParam Long idGiudice) {
+        valutazioneValidator.validateInputUtente(idSottomissione, giudizio, punteggio, idGiudice);
+        Giudice giudice = new Giudice();
+        giudice.setId(idGiudice);
+        return ResponseEntity.ok(valutazioneService.valutaSottomissione(idSottomissione,
+                giudizio, punteggio, giudice));
     }
 }
