456b212f653d3b32e22e6513a999a687406c6e6b
Francesco Di Marzio <francesco.dimarzio@studenti.unicam.it>
2026-04-22 15:55:49 +0200

    Iterazione 4: SottomissioneController.java con annotazioni Spring

--- src/main/java/com/hackhub/controller/SottomissioneController.java
+++ src/main/java/com/hackhub/controller/SottomissioneController.java
@@ -3,37 +3,41 @@ package com.hackhub.controller;
 import com.hackhub.model.*;
 import com.hackhub.service.*;
 import com.hackhub.validator.*;
-import com.hackhub.dto.*;
-
-import com.hackhub.model.Sottomissione;
-import com.hackhub.service.SottomissioneService;
-import com.hackhub.validator.SottomissioneValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RestController
+@RequestMapping("/sottomissione")
 public class SottomissioneController {
 
+    @Autowired
     private SottomissioneService sottomissioneService;
+
+    @Autowired
     private SottomissioneValidator sottomissioneValidator;
 
-    public SottomissioneController(SottomissioneService sottomissioneService,
-                                   SottomissioneValidator sottomissioneValidator) {
-        this.sottomissioneService = sottomissioneService;
-        this.sottomissioneValidator = sottomissioneValidator;
-    }
-
-    public Sottomissione inviaSottomissione(Long idTeam, Long idHackathon,
-                                            String link, Long idLeader) {
+    @PostMapping("/invia")
+    public ResponseEntity<Sottomissione> inviaSottomissione(@RequestParam Long idTeam,
+                                                            @RequestParam Long idHackathon,
+                                                            @RequestParam String link,
+                                                            @RequestParam Long idLeader) {
         sottomissioneValidator.validateInputInvia(idTeam, idHackathon, link, idLeader);
-        return sottomissioneService.inviaSottomissione(idTeam, idHackathon, link, idLeader);
+        return ResponseEntity.ok(sottomissioneService.inviaSottomissione(idTeam, idHackathon, link, idLeader));
     }
 
-    public Sottomissione aggiornaSottomissione(Long idSottomissione,
-                                               String nuovoLink, Long idLeader) {
+    @PutMapping("/{idSottomissione}/aggiorna")
+    public ResponseEntity<Sottomissione> aggiornaSottomissione(@PathVariable Long idSottomissione,
+                                                               @RequestParam String nuovoLink,
+                                                               @RequestParam Long idLeader) {
         sottomissioneValidator.validateInputAggiorna(idSottomissione, nuovoLink, idLeader);
-        return sottomissioneService.aggiornaSottomissione(idSottomissione, nuovoLink, idLeader);
+        return ResponseEntity.ok(sottomissioneService.aggiornaSottomissione(idSottomissione, nuovoLink, idLeader));
     }
 
-    public List<Sottomissione> visualizzaSottomissioni(Long idHackathon, Long idGiudice) {
-        return sottomissioneService.visualizzaSottomissioni(idHackathon, idGiudice);
+    @GetMapping("/{idHackathon}/lista")
+    public ResponseEntity<List<Sottomissione>> visualizzaSottomissioni(@PathVariable Long idHackathon,
+                                                                       @RequestParam Long idGiudice) {
+        return ResponseEntity.ok(sottomissioneService.visualizzaSottomissioni(idHackathon, idGiudice));
     }
 }
