3aca330a38985dd2f8d6fb638f5a4833c06cc09d
mandydb04 <mariadeledibiase04@gmail.com>
2026-04-23 15:15:15 +0200

    Implementazione Repository per getClassificaFinale

--- src/main/java/com/hackhub/repository/IValutazioneRepository.java
+++ src/main/java/com/hackhub/repository/IValutazioneRepository.java
@@ -2,12 +2,15 @@ package com.hackhub.repository;
 
 import com.hackhub.model.Valutazione;
 import com.hackhub.model.Sottomissione;
+import com.hackhub.model.Hackathon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IValutazioneRepository extends JpaRepository<Valutazione, Long> {
     Optional<Valutazione> findBySottomissione(Sottomissione sottomissione);
+    List<Valutazione> findBySottomissioneHackathon(Hackathon hackathon);
 }
